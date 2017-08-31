c9ab08124aab37e9ef400e1e3af6a0b821ae6bad
wildlifehexagon <crossthebreeze@gmail.com>
2017-08-30 21:59:05 -0500

    revise mario.c

--- pset1/mario.c
+++ pset1/mario.c
@@ -1,19 +1,17 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
-int main(void) {
+int main (void) {
     int height;
 
     do {
         printf("Height: ");
         height = get_int();
     }
-    while ((height > 23) || (height < 0));
+    while (height > 23 || height < 0);
 
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < height - i - 1; j++)
-        {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < height - i - 1; j++) {
             printf(" ");
         }
         for (int k = 0; k <= i + 1; k++) {
