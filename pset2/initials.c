7d00ee7776698d3a759c0e3758b94c3ebbc97430
wildlifehexagon <crossthebreeze@gmail.com>
2017-09-17 16:26:07 -0500

    revise initials.c solution

--- pset2/initials.c
+++ pset2/initials.c
@@ -1,17 +1,20 @@
-#include <stdio.h>
 #include <cs50.h>
-#include <string.h>
+#include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
-int main(void) {
+int main (void) {
     string name = get_string();
 
-    printf("%c", toupper(name[0]));
+    if (name != NULL) {
+        printf("%c", toupper(name[0]));
 
-    for (int i = 0; i < strlen(name); i++) {
-        if (name[i] == ' ') {
-            printf("%c", toupper(name[i + 1]));
+        for (int i = 0, n = strlen(name); i < n; i++) {
+            if (name[i] == ' ') {
+                printf("%c", toupper(name[i + 1]));
+            }
         }
+
+        printf("\n");
     }
-    printf("\n");
 }
