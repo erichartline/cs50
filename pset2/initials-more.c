f19214e65731c19f6d448f71eb341eab97e49ca3
wildlifehexagon <crossthebreeze@gmail.com>
2017-09-17 17:11:05 -0500

    add more comfortable initials.c solution

---
+++ pset2/initials-more.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main (void) {
+    string name = get_string();
+
+    if (name != NULL) {
+        if (name[0] != ' ') {
+            printf("%c", toupper(name[0]));
+        }
+
+        for (int i = 0, n = strlen(name); i < n; i++) {
+            if (name[i] == ' ' && name[i + 1] != ' ' && name[i + 1] != '\0') {
+                printf("%c", toupper(name[i + 1]));
+            }
+        }
+
+        printf("\n");
+    }
+}
